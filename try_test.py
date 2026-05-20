b9f3e7b5620c652c0bd77155322bde03b1f0587a
James <p05353498@yahoo.com.tw>
2026-05-20 14:44:33 +0800

    修正dockerfile bug

--- try_test.py
+++ try_test.py
@@ -1,18 +1,29 @@
 from openai import OpenAI
-
+import time
 client = OpenAI(
     base_url="http://llamacpp-server:8080/v1",
     api_key="dummy"
 )
 
-response = client.chat.completions.create(
-    model="Qwen3.5-0.8B-Q8_0.gguf",
-    messages=[
-        {
-            "role": "user",
-            "content": "你好"
-        }
-    ]
-)
 
-print(response.choices[0].message.content)
+while True:
+    try:
+
+        response = client.chat.completions.create(
+            model="Qwen3.5-0.8B-Q8_0.gguf",
+            messages=[
+                {
+                    "role": "user",
+                    "content": "你好"
+                }
+            ]
+        )
+
+        print(response.choices[0].message.content)
+        break
+    
+    except Exception as e:
+        print("Model loading, retrying...")
+        print(e)
+        time.sleep(5)
+
